7826b9b6dd289ea5c2c2dcbfd07a6c8d5dd32ea2
emil.vettainen <emil.vettainen@utb.ecutbildning.se>
2023-11-16 20:10:15 +0100

    added padding to article

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -4,15 +4,18 @@ import ArticleNews from '../home/article_news/ArticleNews'
 import FetchArticlePost from '../../components/fetch_articles/FetchArticlePost'
 import TopSection from '../../components/top_section/TopSection'
 import Footer from '../../components/footer/Footer'
+import './SingleArticle.css'
 
 
 const index = () => {
     return (
         <>
         <Header />
-        <TopSection title="News & Articles" url="news" />
-        <FetchArticlePost />
-        <ArticleNews />
+        <main className='single-article'>
+            <TopSection title="News & Articles" url="news" />
+            <FetchArticlePost />
+            <ArticleNews />
+        </main>
         <Footer />
         </>
     )
